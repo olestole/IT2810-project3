e640d845b8b8ac592189e38ca50638e4dbbb96a9
Ole August <oleastole@gmail.com>
2020-10-07 16:03:46 +0200

    Setup project with a server and GraphQL

---
+++ backend/src/resolvers.js
@@ -0,0 +1,16 @@
+import { Cat } from "./models/Cat";
+
+export const resolvers = {
+  Query: {
+    hello: () => "HALLA BRO",
+    cats: () => Cat.find(),
+  },
+
+  Mutation: {
+    createCat: async (_, { name }) => {
+      const kitty = new Cat({ name });
+      await kitty.save();
+      return kitty;
+    },
+  },
+};
