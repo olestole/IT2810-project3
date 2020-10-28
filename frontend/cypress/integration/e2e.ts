ec6794b5e66f33377e99561f2213320099a8fdb3
Sverre Rynning-Tønnesen <sverrert@MacBook-Pro.local>
2020-10-28 19:54:17 +0100

    #18 trying to add auth0 into tests

--- frontend/cypress/integration/e2e.ts
+++ frontend/cypress/integration/e2e.ts
@@ -3,11 +3,27 @@ export {}; //Makes file turn into a module, added to remove error
 describe('End to end test', () => {
   const email: string = 'testUser123@gmail.com';
   const password: string = 'ValidPassword123';
+  //https://dev-sti0dl03.eu.auth0.com/login?state=g6Fo2SBZNG5iNXhKRDE3QXQzMlpfeEROekI0RFdfZTF2M2JtZKN0aWTZIEpNZTFyT2V0RVN0NmVfQnJhWU5ScmRmczlUalFQZGNGo2NpZNkgc1oxQWJ2N2QzNXdTdk9MSkNkNEdnMkRwZjQ3V2o2d1Y&client=sZ1Abv7d35wSvOLJCd4Gg2Dpf47Wj6wV&protocol=oauth2&audience=https%3A%2F%2Fdev-sti0dl03.eu.auth0.com%2Fapi%2Fv2%2F&scope=openid%20profile%20email%20read%3Acurrent_user%20update%3Acurrent_user_metadata&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fcallback&response_type=code&response_mode=query&nonce=V2tOcVVDNzZDdVBlMFFjTlRqMDEzQ1lfYjFzVndNb1pmbnpneFlPTkZpRw%3D%3D&code_challenge=xN6dyDI_8Ffpe699c_NetEEKP7bGn-8pROl3IoyPAZ8&code_challenge_method=S256&auth0Client=eyJuYW1lIjoiYXV0aDAtcmVhY3QiLCJ2ZXJzaW9uIjoiMS4xLjAifQ%3D%3D
+  const options = {
+    method: 'POST',
+    url: 'https://dev-sti0dl03.eu.auth0.com/oauth/token',
+    body: {
+      grant_type: 'password',
+      username: 'testUser123@gmail.com',
+      password: 'ValidPassword123',
+      audience: 'https://dev-sti0dl03.eu.auth0.com/api/v2/',
+      scope: 'openid profile email',
+      //client_id: '',
+      //client_secret: '',
+      client_id: `${process.env.REACT_APP_AUTH0_CLIENT_ID}`,
+      client_secret: `${process.env.REACT_APP_CLIENT_SECRET}`,
+    },
+  };
 
   beforeEach(() => {
     cy.visit('http://localhost:3000');
   });
-
+  /*
   it('Page is showing', () => {
     cy.contains('WineEncyclopedia');
   });
@@ -55,9 +71,10 @@ describe('End to end test', () => {
   });
 
   it('create user', () => {});
-
+*/
   it('Comment item', () => {
     //create function for login
+    cy.request(options);
     cy.get('#searchField').type('bad santa{enter}');
     cy.contains('Bad Santa Juleakevitt').should('be.visible').click();
     cy.get('#loginForReviewButton').click();
