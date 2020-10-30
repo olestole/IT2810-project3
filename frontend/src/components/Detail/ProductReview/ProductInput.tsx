a05d1763d50501ff2c3c56e7c3bfdd67bc05e693
Ole August <oleastole@gmail.com>
2020-10-30 16:52:36 +0100

    fix: update the snapshot-tests

--- frontend/src/components/Detail/ProductReview/ProductInput.tsx
+++ frontend/src/components/Detail/ProductReview/ProductInput.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles(() =>
